9defc02a9fcc743e2bc0dd83c4cef81c297aef00
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-02-17 02:05:15 -0800

    add server config

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/cityos-dev/Qiao-Lin/database"
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,7 +11,10 @@ func main() {
 	database.ConnectToPostgresDb()
 
 	app := fiber.New(fiber.Config{
-		BodyLimit: 10 * 1024 * 1024, // this is the default limit of 10MB
+		BodyLimit:    10 * 1024 * 1024, // this is the default limit of 10MB
+		IdleTimeout:  20 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
 	})
 	setupRoutes(app)
 
