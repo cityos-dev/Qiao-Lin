1b8928f322b1fbbb95c54d2edb8d0f4e0ce65409
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-02-18 17:33:08 -0800

    enable server CORS try

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/cityos-dev/Qiao-Lin/database"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -19,6 +20,14 @@ func main() {
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	})
+
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
+
 	setupRoutes(app)
 	fmt.Println("server started ")
 	app.Listen("0.0.0.0:8080")
