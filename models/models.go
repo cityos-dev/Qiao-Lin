dd49b413e7729c4e175df3e3f58e8d62507b1675
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-02-22 02:36:36 -0800

    fix return uuid not name issue

--- models/models.go
+++ models/models.go
@@ -7,8 +7,8 @@ import (
 )
 
 type File struct {
-	FileId     uuid.UUID `json:"fileid" gorm:"string:not null:default:null"`
 	Name       string    `json:"name" gorm:"string:not null:default:null"`
+	FileId     uuid.UUID `json:"fileid" gorm:"string:not null:default:null"`
 	Size       int64     `json:"size" gorm:"int:not null:default:null"`
 	Created_At time.Time `json:"created_at"`
 }
